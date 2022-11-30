bf8850bcdd84937bdca44ce6ad26c392a9016f66
yuck <60288171+yuckdevchan@users.noreply.github.com>
2022-11-30 22:23:14 +0000

    Update jellyfin-backup.sh

--- jellyfin-backup.sh
+++ jellyfin-backup.sh
@@ -2,8 +2,30 @@
 
 echo "Jellyfin Backup Script"
 
-DATA=$JELLYFIN_DATA_DIR
-CONFIG=$JELLYFIN_CONFIG_DIR
-CACHE=$JELLYFIN_CACHE_DIR
-WEB=$JELLYFIN_WEB_DIR
-LOGS=$JELLYFIN_LOG_DIR
+#Directory environment variables:
+
+#DATA=$JELLYFIN_DATA_DIR
+#CONFIG=$JELLYFIN_CONFIG_DIR
+#CACHE=$JELLYFIN_CACHE_DIR
+#WEB=$JELLYFIN_WEB_DIR
+#LOGS=$JELLYFIN_LOG_DIR
+
+start=$SECONDS
+
+sudo rm -rf /home/$USER/backup
+echo "Deleted old backups"
+mkdir /home/$USER/backup
+echo "Made new backup folder"
+sudo cp $JELLYFIN_DATA_DIR /home/$USER/backup/data
+echo "Backed up data directory"
+sudo cp $JELLYFIN_CONFIG_DIR /home/$USER/backup/config
+echo "Backed up config directory"
+sudo cp $JELLYFIN_CACHE_DIR /home/$USER/backup/cache
+echo "Backed up cache directory"
+sudo cp $JELLYFIN_WEB_DIR /home/$USER/backup/web
+echo "Backed up web directory"
+sudo cp $JELLYFIN_LOG_DIR /home/$USER/backup/logs
+echo "Backed up log directory"
+echo "Done!
+
+duration=$(( SECONDS - start ))
